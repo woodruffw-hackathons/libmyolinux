0655a3702b64644a7bcaa0a07372a0eae260a44d
William Woodruff <william@tuffbizz.com>
2014-09-27 18:42:06 -0400

    commit @ Sat Sep 27 18:42:06 EDT 2014

--- src/pi/accept-myo-data.py
+++ src/pi/accept-myo-data.py
@@ -2,6 +2,17 @@
 # accept-myo-data.py
 
 import socket
+import struct
+
+POSES = {
+0 : "rest",
+1 : "fist",
+2 : "waveIn",
+3 : "waveOut",
+4 : "fingersSpread",
+5 : "reserved1",
+6 : "thumbToPinky",
+}
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('', 6969))
@@ -9,7 +20,8 @@ sock.listen(1)
 conn, addr = sock.accept()
 
 while 1:
-    data = conn.recv(1024)
+    data = struct.unpack("fffffffBB", conn.recv(30))
+    motion = POSES.get(conn[7], 'unknown')
     if not data: break
     print data
 conn.close()
