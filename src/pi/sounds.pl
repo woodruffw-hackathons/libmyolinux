7e8957588c4599a6db85a29280a894047f6915ec
William Woodruff <william@tuffbizz.com>
2014-09-27 20:15:21 -0400

    commit @ Sat Sep 27 20:15:21 EDT 2014

--- src/pi/sounds.pl
+++ src/pi/sounds.pl
@@ -4,19 +4,22 @@ my $lc = 0;
 while (<>) {
 	if ($lc % 5) {
 		if ($_ eq "rest") {
-			
+
 		}
 		elsif ($_ eq "waveIn") {
-			`amixer -D pulse sset Master 5%+`;
+			#`amixer -D pulse sset Master 5%+`;
+			print "wi\n";
 		}
 		elsif ($_ eq "waveOut") {
-			`amixer -D pulse sset Master 5%-`;
+			#`amixer -D pulse sset Master 5%-`;
+			print "wo\n";
 		}
 		elsif ($_ eq "fist") {
-			`amixer set master Mute`;
+			#`amixer set master Mute`;
+			print "fist\n";
 		}
 		else {
-			`amixer set master Unmute`;
+			#`amixer set master Unmute`;
 		}
 	}
 	$lc++;
