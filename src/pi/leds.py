489f0df1a4a451c13a1930a3d7c18236a2399425
William Woodruff <william@tuffbizz.com>
2014-09-27 21:18:27 -0400

    fix @ Sat Sep 27 21:18:27 EDT 2014

--- src/pi/leds.py
+++ src/pi/leds.py
@@ -4,9 +4,9 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7)
-GPIO.setup(11)
-GPIO.setup(12)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 GPIO.output(7, GPIO.HIGH)
 GPIO.output(11, GPIO.HIGH)
 GPIO.output(12, GPIO.HIGH)
