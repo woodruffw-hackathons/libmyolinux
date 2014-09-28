b50eb2f28aa4248e73c817fa4d73d23d0e9abcc1
William Woodruff <william@tuffbizz.com>
2014-09-27 21:44:01 -0400

    fix @ Sat Sep 27 21:44:01 EDT 2014

--- src/pi/leds.py
+++ src/pi/leds.py
@@ -8,13 +8,10 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.cleanup()
 GPIO.setup(7, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 
 while True:
 	GPIO.output(7, GPIO.HIGH)
-	time.sleep(1)
-	GPIO.output(11, GPIO.HIGH)
 	time.sleep(1)
 	GPIO.output(12, GPIO.HIGH)
 	time.sleep(1)
