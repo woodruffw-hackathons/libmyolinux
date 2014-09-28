c8774a53ee6f355e88926b25392472f4a8267cc6
William Woodruff <william@tuffbizz.com>
2014-09-28 11:01:05 -0400

    commit @ Sun Sep 28 11:01:05 EDT 2014

--- src/libmyolinux/game1.py
+++ src/libmyolinux/game1.py
@@ -8,122 +8,141 @@ import sys
 import os
 import tempfile
 import subprocess
-import threading
+import thread
 
 from sys import stdin
 
-class Game(threading.Thread):
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+BLUE = (0, 0, 255)
 
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GREEN = ( 0, 255, 0)
-    RED = ( 255, 0, 0)
-    BLUE = (0, 0, 255)
+PI = 3.1415
 
-    PI = 3.1415
+#---Main Program Loop---
+def game():
+    global last_pose
+    pygame.init()
 
-    #---Main Program Loop---
-    def game():
-        global last_pose
-        pygame.init()
-
-        size = (700, 500)
-        screen = pygame.display.set_mode(size)
-        pygame.display.set_caption("Myo Linux Driver Demo")
+    size = (700, 500)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Myo Linux Driver Demo")
 
 
-        x_coord = 0
-        y_coord = 0
-        height = 50
-        width = 50
+    x_coord = 0
+    y_coord = 0
+    height = 50
+    width = 50
 
-        x_speed = 0
-        y_speed = 0
-        out_speed = 0
+    x_speed = 0
+    y_speed = 0
+    out_speed = 0
+    
+    done = False
+    pressed = False
+
+    clock = pygame.time.Clock()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            # User pressed down on a key
+            if event.type == pygame.KEYDOWN:
+                # Change Position
+                if event.key == pygame.K_LEFT:
+                    x_speed = -3
+                if event.key == pygame.K_RIGHT:
+                    x_speed = 3
+                if event.key == pygame.K_UP:
+                    y_speed = -3
+                if event.key == pygame.K_DOWN:
+                    y_speed = 3
+                # Change Size
+                if event.key == pygame.K_1:
+                    out_speed = 3
+                if event.key == pygame.K_2:
+                    out_speed = -3
+            # User let up on a key
+            if event.type == pygame.KEYUP:
+                # If it is an arrow key, reset vector back to zero
+                if event.key == pygame.K_LEFT:
+                    x_speed = 0
+                if event.key == pygame.K_RIGHT:
+                    x_speed = 0
+                if event.key == pygame.K_UP:
+                    y_speed = 0
+                if event.key == pygame.K_DOWN:
+                    y_speed = 0
+                if event.key == pygame.K_1:
+                    out_speed = 0
+                if event.key == pygame.K_2:
+                    out_speed = 0
+                    
+        #---Drawing code should go here---
+  
+        #last_pose = myo.getPose()
+        #Clear Screen
+        screen.fill(WHITE)
+    
+        # Move the object according to the speed vector.
+        x_coord += x_speed
+        y_coord += y_speed
+        height += out_speed
+        width += out_speed
         
-        done = False
-        pressed = False
+        pygame.draw.rect(screen, BLUE, [x_coord, y_coord, width, height])
+        #update screen
+        pygame.display.flip()
 
-        clock = pygame.time.Clock()
+        #limit to 60 frames per second
+        clock.tick(60)
+    pygame.quit()
 
-        while not done:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    done = True
-                # User pressed down on a key
-                if event.type == pygame.KEYDOWN:
-                    # Change Position
-                    if event.key == pygame.K_LEFT:
-                        x_speed = -3
-                    if event.key == pygame.K_RIGHT:
-                        x_speed = 3
-                    if event.key == pygame.K_UP:
-                        y_speed = -3
-                    if event.key == pygame.K_DOWN:
-                        y_speed = 3
-                    # Change Size
-                    if event.key == pygame.K_1:
-                        out_speed = 3
-                    if event.key == pygame.K_2:
-                        out_speed = -3
-                # User let up on a key
-                if event.type == pygame.KEYUP:
-                    # If it is an arrow key, reset vector back to zero
-                    if event.key == pygame.K_LEFT:
-                        x_speed = 0
-                    if event.key == pygame.K_RIGHT:
-                        x_speed = 0
-                    if event.key == pygame.K_UP:
-                        y_speed = 0
-                    if event.key == pygame.K_DOWN:
-                        y_speed = 0
-                    if event.key == pygame.K_1:
-                        out_speed = 0
-                    if event.key == pygame.K_2:
-                        out_speed = 0
-                        
-            #---Drawing code should go here---
-      
-            #last_pose = myo.getPose()
-            #Clear Screen
-            screen.fill(WHITE)
-        
-            # Move the object according to the speed vector.
-            x_coord += x_speed
-            y_coord += y_speed
-            height += out_speed
-            width += out_speed
-            
-            pygame.draw.rect(screen, BLUE, [x_coord, y_coord, width, height])
-            #update screen
-            pygame.display.flip()
+'''
+def printData(myo):
 
-            #limit to 60 frames per second
-            clock.tick(60)
-        pygame.quit()
+    global last_pose
+    pose_str = myo.getPoseString()
 
-    '''
-    def printData(myo):
+    print pose_str
+    if (pose_str == "waveOut") and (last_pose != myo.getPose()):
+        myo.vibrate(Myo.VIBE_MEDIUM)
+  
+    last_pose = myo.getPose()
+'''
 
-        global last_pose
-        pose_str = myo.getPoseString()
+def printstdin():
+    POSES = {
+    0 : "rest",
+    1 : "fist",
+    2 : "waveIn",
+    3 : "waveOut",
+    4 : "fingersSpread",
+    5 : "reserved1",
+    6 : "thumbToPinky",
+    }
 
-        print pose_str
-        if (pose_str == "waveOut") and (last_pose != myo.getPose()):
-            myo.vibrate(Myo.VIBE_MEDIUM)
-      
-        last_pose = myo.getPose()
-    '''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', 6969))
+    sock.listen(1)
+    conn, addr = sock.accept()
 
-    def printstdin():
-        threading.Thread.__init__()
-        proc = subprocess.Popen("./example", bufsize=0, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-
-        while True: 
-        	print proc.stdout.readline()
+    try:
+        while 1:
+            data = struct.unpack("fffffffBB", conn.recv(30))
+            motion = POSES.get(data[7], 'unknown')
+            if not data: break
+            print motion
+    except Exception, e:
+        pass
+    finally:
+        conn.close()
+        sock.close()
 
 def main():
-        Game.printstdin()
+    printstdin() 
 
 if __name__ == "__main__":
     main()
